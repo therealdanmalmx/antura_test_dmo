d184ba24519a0f4bd7c790b9ed148f0193f56370
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2024-12-05 20:36:21 +0100

    Remove dissfunctional test

--- vitest.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vitest/config";
-export default defineConfig({
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./src/test/setup.ts", // optional
-    include: ["**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-  },
-});
